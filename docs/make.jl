cc8c2d6a4e24d5c3be2e51192848c177f0cbb186
Anton Oresten <antonoresten@gmail.com>
2025-03-13 18:20:17 +0100

    Add docs

---
+++ docs/make.jl
@@ -0,0 +1,29 @@
+using DynamicStructs
+using Documenter
+
+DocMeta.setdocmeta!(
+    DynamicStructs,
+    :DocTestSetup,
+    :(using DynamicStructs);
+    recursive=true,
+)
+
+makedocs(;
+    modules=[DynamicStructs],
+    authors="Anton Oresten <antonoresten@gmail.com> and contributors",
+    sitename="DynamicStructs.jl",
+    format=Documenter.HTML(;
+        canonical="https://antonoresten.github.io/DynamicStructs.jl",
+        edit_link="main",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+    ],
+    doctest=true,
+)
+
+deploydocs(;
+    repo="github.com/antonoresten/DynamicStructs.jl",
+    devbranch="main",
+)
